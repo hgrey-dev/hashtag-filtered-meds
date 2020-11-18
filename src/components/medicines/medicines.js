e74388cf5d3396c1ce72e7668ab4c743c758e020
hgrey-dev <67564128+hgrey-dev@users.noreply.github.com>
2020-11-19 09:08:27 +1300

    formated to make code more readable

--- src/components/medicines/medicines.js
+++ src/components/medicines/medicines.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import styles from './medicines.module.css';
+import React from "react";
+import styles from "./medicines.module.css";
+
+
 const Medicines = ({ medicines }) => {
-    return (<div className={styles.root}>
-        looks like you have {medicines.length} medicines!
-    </div>);
-}
+  return (
+    <div className={styles.root}>
+      looks like you have {medicines.length} medicines!
+    </div>
+  );
+};
 
 export default Medicines;
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import styles from './search.module.css';
+import React from "react";
+import styles from "./search.module.css";
+
+
 const Search = (props) => {
-    return (<div className={styles.root}>
-        <input aria-label="search" className={styles.search} placeholder="filter meds" type="search"></input>
-    </div>);
-}
+  return (
+    <div className={styles.root}>
+      <input
+        aria-label="search"
+        className={styles.search}
+        placeholder="filter meds"
+        type="search"
+      ></input>
+    </div>
+  );
+};
 
 export default Search;
