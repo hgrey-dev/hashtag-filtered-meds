49f8e8a7b4b81c2e82a90d8dfd52d4b92f665c55
hgrey-dev <67564128+hgrey-dev@users.noreply.github.com>
2020-11-19 14:42:19 +1300

    maped data. msges change depending of data length

--- src/components/medicines/medicines.js
+++ src/components/medicines/medicines.js
@@ -1,12 +1,26 @@
 import React from "react";
 import styles from "./medicines.module.css";
-
+import Card from "../Card/Card";
 
 const Medicines = ({ medicines }) => {
   return (
-    <div className={styles.root}>
-      looks like you have {medicines.length} medicines!
-    </div>
+    <>
+      <div className={styles.root}>
+        {
+        medicines.length == 1 && <div>looks like you have {medicines.length} medicine!</div>
+        }
+        {
+        medicines.length == 0 && <div>looks like you have no medicine!</div>}
+        {
+        medicines.length >= 2 && <div>looks like you have {medicines.length} medicines!</div>
+        }
+      </div>
+      <div>
+        {medicines.map((m, i) => 
+          <Card name={m.name} key={i} company={m.company} />
+        )}
+      </div>
+    </>
   );
 };
 
