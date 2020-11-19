20f8a185a112b958b06cd91127c20e90e59bb798
hgrey-dev <67564128+hgrey-dev@users.noreply.github.com>
2020-11-19 14:39:40 +1300

    created seperate card component for reusability

---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from "react";
+const mystlez = {
+  padding: "15px",
+  margin: "15px",
+  border: "none",
+  background: "peachpuff",
+};
+const Card = (props) => {
+  return (
+    <div style={mystlez}>
+      <h3>{props.name}</h3>
+      <p>{props.company}</p>
+    </div>
+  );
+};
+
+export default Card;
