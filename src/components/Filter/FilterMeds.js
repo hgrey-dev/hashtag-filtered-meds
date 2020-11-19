ddfeaef08c3013242b7603567d5ba3d58d6eff52
hgrey-dev <67564128+hgrey-dev@users.noreply.github.com>
2020-11-20 12:54:17 +1300

    added seperate filter component

---
+++ src/components/Filter/FilterMeds.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FilteredMedsList = ({medicines}) => {
+
+    
+return(
+    <>
+    <div>
+    hello
+    </div>
+    </>
+)    
+}
+
+export default FilteredMedsList
