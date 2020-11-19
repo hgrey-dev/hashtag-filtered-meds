c015f21287d7e0966c5a0f2e8d7dd496fe845b04
hgrey-dev <67564128+hgrey-dev@users.noreply.github.com>
2020-11-20 12:53:38 +1300

    edited txt

--- src/components/medicines/medicines.js
+++ src/components/medicines/medicines.js
@@ -10,7 +10,7 @@ const Medicines = ({ medicines }) => {
         medicines.length === 1 && <div>looks like you have {medicines.length} medicine!</div>
         }
         {
-        medicines.length === 0 && <div>looks like you have no medicine!</div>}
+        medicines.length === 0 && <div>looks like you dont have any medicines!</div>}
         {
         medicines.length >= 2 && <div>looks like you have {medicines.length} medicines!</div>
         }
