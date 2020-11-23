3b70419078362af385c461aab38847b71ab09457
gravitygrey-dev <hayden_grey@hotmail.co.nz>
2020-11-23 20:29:30 +1300

    adjusted css and header for mobile

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./header.module.css";
 
-
 const Header = (props) => {
   return (
     <>
       <header className={styles.header}>
-        #no-filter meds store
-        {props.children}
+        <h3 className={styles.h3}>
+          #no-filter meds store
+          </h3>
+       <p className={styles.p}>{props.children}</p>
+        
       </header>
     </>
   );
