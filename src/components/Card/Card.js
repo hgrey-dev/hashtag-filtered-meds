7a903b2cc603ee4626b6925b3bc6d9ac579db6c1
gravitygrey-dev <hayden_grey@hotmail.co.nz>
2020-11-24 09:52:10 +1300

    updated css for mobile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,10 @@
 // create reusable card component with styling
 import React from "react";
-const mystlez = {
-  padding: "15px",
-  margin: "15px",
-  border: "none",
-  background: "peachpuff",
-};
+import styles from "./cards.module.css";
+
 const Card = (props) => {
   return (
-    <div style={mystlez}>
+    <div className={styles.mystlez}>
       <h3>{props.name}</h3>
       <p>{props.company}</p>
     </div>
